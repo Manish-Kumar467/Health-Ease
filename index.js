f8ee26540c15e875e81d7a729fe16d4b84cb2ce5
Manish-Kumar467 <mkwork467@gmail.com>
2024-10-03 10:08:01 +0530

    .

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
+env.config(); // 4
+
+// 3
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // 4
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // for using static css and image files 
 app.use(express.static("public"));
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "healthease",
-  password: "3001work",
-  port: 5432
+// 3
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = new pg.Client({ // 4
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 db.connect();
@@ -26,12 +45,59 @@ app.get("/", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  res.render("login.ejs");
+  const errorMessage = req.query.error === 'already_registered' ? 'This email is already registered. Please login.' : null;
+  res.render("login.ejs", { errorMessage });
 });
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
+
+// 3
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+// 3
+app.get("/success", (req, res) => {
+  // console.log(req.user);
+  if (req.isAuthenticated()) {
+    res.render("index.ejs");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//4
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// 4
+app.get(
+  "/auth/google/success",
+  passport.authenticate("google", {
+    successRedirect: "/success",
+    failureRedirect: "/login",
+  })
+);
+
+// 3
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/success",
+    failureRedirect: "/login",
+  })
+);
 
 app.post("/register", async (req, res) => {
   const email = req.body.username;
@@ -43,19 +109,22 @@ app.post("/register", async (req, res) => {
     ]);
 
     if (checkResult.rows.length > 0) {
-      res.send("Email already exists. Try logging in.");
+      res.redirect(`/login?error=already_registered`); // 3
     } else {
       //hashing the password and saving it in the database
       bcrypt.hash(password, saltRounds, async (err, hash) => {
         if (err) {
           console.error("Error hashing password:", err);
-        } else {
-          console.log("Hashed Password:", hash);
-          await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+        } else { // 3
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
             [email, hash]
           );
-          res.render("index.ejs");
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("success");
+            res.redirect("/success");
+          });
         }
       });
     }
@@ -64,6 +133,42 @@ app.post("/register", async (req, res) => {
   }
 });
 
+// 3
+passport.use(
+  "local",
+  new Strategy(async function verify(username, password, cb) {
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            //Error with password check
+            console.error("Error comparing passwords:", err);
+            return cb(err);
+          } else {
+            if (valid) {
+              //Passed password check
+              return cb(null, user);
+            } else {
+              //Did not pass password check
+              return cb(null, false);
+            }
+          }
+        });
+      } else {
+        return cb("User not found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
+
+/*
 app.post("/login", async (req, res) => {
   const email = req.body.username;
   const loginPassword = req.body.password;
@@ -93,6 +198,47 @@ app.post("/login", async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+*/
+
+// 5
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/success",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        console.log(profile);
+        const result = await db.query("SELECT * FROM users WHERE email = $1", [
+          profile.email,
+        ]);
+        if (result.rows.length === 0) {
+          const newUser = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            [profile.email, "google"]
+          );
+          return cb(null, newUser.rows[0]);
+        } else {
+          return cb(null, result.rows[0]);
+        }
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  )
+);
+
+// 3
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
 });
 
 app.listen(port, () => {
